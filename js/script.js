ff3149b6d97eb508f0776da0efdb581d415c0f90
Will <wjreed2@gmail.com>
2019-06-15 12:23:20 -0600

    totally changed haha

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("sidebar").style.width = "160px";
+  document.getElementById("main").style.marginLeft = "160px";
+}
+
+function closeNav() {
+  document.getElementById("sidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
