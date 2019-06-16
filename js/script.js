f0ad1d0006ed25d85524bc4bf751b5a2a18358ab
Will <wjreed2@gmail.com>
2019-06-15 19:32:22 -0600

    hopefully js script works now? and only one button!

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 function openNav() {
-  document.getElementById("sidebar").style.width = "160px";
-  document.getElementById("main").style.marginLeft = "160px";
+  document.getElementById("sidebar").style.height = "63px";
+  document.getElementById("main").style.marginTop = "83px";
 }
 
 function closeNav() {
-  document.getElementById("sidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("sidebar").style.height = "0";
+  document.getElementById("main").style.marginTop= "0";
+}
+
+function change() {
+  var change = document.getElementById("openbtn");
+  if (change.innerHTML == "↓")
+  {
+      change.innerHTML = "↑";
+      document.getElementById("openbtn").setAttribute("onclick", "javascript: change(); closeNav();")
+  }
+  else {
+      change.innerHTML = "↓";
+      document.getElementById("openbtn").setAttribute("onclick", "javascript: change(); openNav();")
+  }
 }
